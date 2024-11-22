b4cbeb00095d283d91ea532e90a97a9d0a6664ea
paulosxx <10xsala1@gmail.com>
2024-11-22 10:17:56 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Boi',
+    'Qual a melhor parte do boi?',
+    'Segundo o Google, o Acém!'
+)
+
+criaCartao(
+    'Açougue',
+    'O que faz um Açougueiro nível 3?',
+    'Acompanha o recebimento da carne, verifica sua quantidade e qualidade, armazena em locais apropriados, retalha as peças conforme necessidade, faz o pré-preparo, utilizando equipamentos e técnicas adequadas.'
+)
+
+criaCartao(
+    'Corte',
+    'Qual é o melhor objeto para cortar?',
+    'Um cutelo, ou uma facaaaaaaaaa!!!'
+)
+
+criaCartao(
+    'Outros',
+    'Quem é a melhor pessoa do universo?',
+    'O phhhhhhhhhhhhhh!!!'
+)
